dcc34ba4c683306a6651a7f48b9b225238f08208
Resta <restasabrina22@gmail.com>
2025-05-08 09:20:13 +0700

    UTS PEMKOM2 MONGODB

---
+++ src/main/java/com/mycompany/p_uts_23090134_c_2025/CRUD_23090134_C_2025.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.p_uts_23090134_c_2025;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CRUD_23090134_C_2025 {
+    
+}
---
+++ src/main/java/com/mycompany/p_uts_23090134_c_2025/P_UTS_23090134_C_2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p_uts_23090134_c_2025;
+
+/**
+ *
+ * @author ASUS
+ */
+public class P_UTS_23090134_C_2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
